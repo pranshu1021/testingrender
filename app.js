7c9708fd3ae2167f3499f6c022f1080b37d1e938
Pranshu Gupta <pranshugupta1423@gmail.com>
2026-03-12 19:48:48 +0530

    First change

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const path= require("path");
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+
+const db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
+});
+
+app.get("/home",(req,res)=>{
+    res.render("home.ejs");
+})
+
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
+});

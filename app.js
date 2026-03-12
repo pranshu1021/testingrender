a0ebdbbe56d406d92b94802637ea0e3804244397
Pranshu Gupta <pranshugupta1423@gmail.com>
2026-03-12 21:19:09 +0530

    updated db

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 
@@ -18,12 +17,23 @@ const db = mysql.createConnection({
     database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT
 });
+
 app.get("/",(req,res)=>{
-    db.query("SELECT * FROM users",(err,result)=>{
-        console.log(result)
+        db.query("SELECT * FROM users",(err,result)=>{
         res.render("home.ejs",{result});
     })
-    
+})
+
+app.post("/",(req,res)=>{
+    let {id,name,age} = req.body;
+    db.query(
+        "INSERT INTO users (id, username, age) VALUES (?, ?, ?)",
+        [id, name, age],
+        (err,result)=>{
+    console.log("New user added");
+    res.redirect("/");
+});
+    console.log(id,name,age)
 })
 app.listen(port,()=>{
     console.log(`listening to port ${port}`);

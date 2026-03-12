ad5913fbacfb948ec42d884b31bcdf0535efd7db
Pranshu Gupta <pranshugupta1423@gmail.com>
2026-03-12 21:28:10 +0530

    new changes

--- app.js
+++ app.js
@@ -18,23 +18,31 @@ const db = mysql.createConnection({
     port: process.env.MYSQLPORT
 });
 
-app.get("/",(req,res)=>{
-        db.query("SELECT * FROM users",(err,result)=>{
-        res.render("home.ejs",{result});
-    })
+db.query("SELECT * FROM users",(err,result)=>{
+    if(err){
+        console.log(err);
+        return res.send("Database error");
+    }
+    res.render("home.ejs",{result});
 })
 
 app.post("/",(req,res)=>{
     let {id,name,age} = req.body;
+
     db.query(
         "INSERT INTO users (id, username, age) VALUES (?, ?, ?)",
         [id, name, age],
         (err,result)=>{
-    console.log("New user added");
-    res.redirect("/");
+            if(err){
+                console.log(err);
+                return res.send("Insert error");
+            }
+
+            console.log("New user added");
+            res.redirect("/");
+        }
+    );
 });
-    console.log(id,name,age)
-})
 app.listen(port,()=>{
     console.log(`listening to port ${port}`);
 });
